12ead35cbd519fd97dc4bd839a94f72bdff48fe7
Matheus Pimenta <matheuscscp@gmail.com>
2025-08-06 01:32:54 +0100

    Onboard spotify-shuffler
    Signed-off-by: Matheus Pimenta <matheuscscp@gmail.com>

--- serve_for_me.go
+++ serve_for_me.go
@@ -4,6 +4,7 @@ import (
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 
 	"github.com/matheuscscp/serve-for-me/serveforme"
+	serveformeapi "github.com/matheuscscp/serve-for-me/serveforme/api"
 )
 
 func init() {
@@ -13,6 +14,12 @@ func init() {
 			Issuer:   "https://accounts.google.com",
 			ClientID: "764086051850-6qr4p6gpi6hn506pt8ejuq83di341hur.apps.googleusercontent.com",
 			Subject:  "105001485022452358114",
+		},
+		// Environment shuffle of github.com/matheuscscp/spotify-shuffler.
+		{
+			Issuer:   "https://token.actions.githubusercontent.com",
+			ClientID: serveformeapi.ClientID,
+			Subject:  "repo:matheuscscp/spotify-shuffler:environment:shuffle",
 		},
 	})
 	if err != nil {
